69eedab5bbaca2fe39a2115eeb172bdb7887fb52
Santos Bogo <72747461+santosbogo@users.noreply.github.com>
2024-06-25 01:34:56 -0300

    Adding print tests

--- src/test/java/edu/austral/ingsis/math/PrintTest.java
+++ src/test/java/edu/austral/ingsis/math/PrintTest.java
@@ -3,15 +3,19 @@ package edu.austral.ingsis.math;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import edu.austral.ingsis.math.functions.*;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class PrintTest {
+  Function function;
 
   /** Case 1 + 6 */
   @Test
   public void shouldPrintFunction1() {
+    function = new Sum(List.of(new Number(1), new Number(6)));
     final String expected = "1 + 6";
-    final String result = expected;
+    final String result = function.print();
 
     assertThat(result, equalTo(expected));
   }
@@ -19,8 +23,9 @@ public class PrintTest {
   /** Case 12 / 2 */
   @Test
   public void shouldPrintFunction2() {
+    function = new Division(new Number(12), new Number(2));
     final String expected = "12 / 2";
-    final String result = expected;
+    final String result = function.print();
 
     assertThat(result, equalTo(expected));
   }
@@ -28,8 +33,10 @@ public class PrintTest {
   /** Case (9 / 2) * 3 */
   @Test
   public void shouldPrintFunction3() {
-    final String expected = "(9 / 2) * 3";
-    final String result = expected;
+    function = new Product(List.of(new Division(new Number(9), new Number(2)), new Number(3)));
+
+    final String expected = "9 / 2 * 3";
+    final String result = function.print();
 
     assertThat(result, equalTo(expected));
   }
@@ -37,26 +44,11 @@ public class PrintTest {
   /** Case (27 / 6) ^ 2 */
   @Test
   public void shouldPrintFunction4() {
-    final String expected = "(27 / 6) ^ 2";
-    final String result = expected;
+    Function division = new Division(new Number(27), new Number(6));
+    function = new Power(division, new Number(2));
 
-    assertThat(result, equalTo(expected));
-  }
-
-  /** Case |value| - 8 */
-  @Test
-  public void shouldPrintFunction6() {
-    final String expected = "|value| - 8";
-    final String result = expected;
-
-    assertThat(result, equalTo(expected));
-  }
-
-  /** Case |value| - 8 */
-  @Test
-  public void shouldPrintFunction7() {
-    final String expected = "|value| - 8";
-    final String result = expected;
+    final String expected = "27 / 6 ^ 2";
+    final String result = function.print();
 
     assertThat(result, equalTo(expected));
   }
@@ -64,8 +56,11 @@ public class PrintTest {
   /** Case (5 - i) * 8 */
   @Test
   public void shouldPrintFunction8() {
-    final String expected = "(5 - i) * 8";
-    final String result = expected;
+    function =
+        new Product(
+            List.of(new Difference(List.of(new Number(5), new Variable("i"))), new Number(8)));
+    final String expected = "5 - i * 8";
+    final String result = function.print();
 
     assertThat(result, equalTo(expected));
   }
